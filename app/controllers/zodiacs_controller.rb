b78e7cdb909ab9048bec511f163c1e82c9690b33
Raghu Betina <rbetina@github.com>
2015-05-06 19:33:11 -0500

    Completes create row using database

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -2,6 +2,13 @@ class ZodiacsController < ApplicationController
   def new_form
   end
   def create_row
+
+    z = Zodiac.new
+    z.sign = params[:symbol]
+    z.creature = params[:animal]
+    z.fortune = params[:horoscope]
+    z.save
+
     @sign = params[:symbol]
     @creature = params[:animal]
     @fortune = params[:horoscope]
