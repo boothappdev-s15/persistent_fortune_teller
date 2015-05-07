2c9863581c52d3de4eda269799332615f0f0cd2b
Raghu Betina <rbetina@github.com>
2015-05-06 21:01:36 -0500

    Adds class code

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -1,16 +1,48 @@
 class ZodiacsController < ApplicationController
+
+  def destroy_row
+    z = Zodiac.find(params[:the_id])
+    z.destroy
+
+    redirect_to("http://localhost:3000/zodiacs")
+  end
+
+  def index
+    @list_of_zodiacs = Zodiac.all
+  end
+
+  def sign
+    @zodiac = Zodiac.find_by({ :sign => params[:the_sign] })
+
+    render('show.html.erb')
+  end
+
+
+  def creature
+    @zodiac = Zodiac.find_by({ :creature => params[:the_creature] })
+
+    render('show.html.erb')
+  end
+
+  def show
+    @zodiac = Zodiac.find_by({ :id => params[:the_id] })
+
+    render('show.html.erb')
+  end
+
   def new_form
   end
   def create_row
-
     z = Zodiac.new
-    z.sign = params[:symbol]
-    z.creature = params[:animal]
+    z.sign = params[:symbol].downcase
+    z.creature = params[:animal].downcase
     z.fortune = params[:horoscope]
     z.save
 
-    @sign = params[:symbol]
-    @creature = params[:animal]
-    @fortune = params[:horoscope]
+    redirect_to('http://localhost:3000/zodiacs')
   end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,15 @@ Rails.application.routes.draw do
   get("/zodiacs/new", { :controller => "zodiacs", :action => "new_form" })
   get("/create_zodiac", { :controller => "zodiacs", :action => "create_row" })
 
+  get("/zodiacs", { :controller => "zodiacs", :action => "index" })
+  get("/delete_zodiac/:the_id", { :controller => "zodiacs", :action => "destroy_row" })
+
+
 
   # VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
-  # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
+  get("/zodiacs/:the_id",         { :controller => "zodiacs", :action => "show" })
 end
